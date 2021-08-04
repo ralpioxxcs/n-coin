ce8a42ee1f5e151a154fd2489ba6499b2a26ab62
ralpioxxcs <tmfmfkal1123@naver.com>
2021-08-04 23:46:44 +0900

    block chain

--- main.go
+++ main.go
@@ -1,8 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"crypto/sha256"
+	"fmt"
+)
+
+type block struct {
+	data     string
+	hash     string
+	prevHash string
+}
+
+/*
+
+[Blocks]
+
+Block1
+	b1Hash = (data + "")
+
+Block2
+	b2Hash = (data + b1Hash)
+
+Block3
+	b3Hash = (data + b2Hash)
+
+	.
+	.
+	.
+
+if Block1's hash is changed, block2 hash is changed,,
+
+*/
 
 func main() {
-	fmt.Println("no coin")
+
+	genesisBlock := block{"Gensis Block", "", ""}
+	//genesisBlock.hash = sha256.Sum256([]byte(genesisBlock.data + genesisBlock.prevHash))
+
+	hash := sha256.Sum256([]byte(genesisBlock.data + genesisBlock.prevHash))
+	hexHash := fmt.Sprintf("%x", hash)
+	genesisBlock.hash = hexHash
+
+	fmt.Println(genesisBlock)
 
 }
---
+++ person/person.go
@@ -0,0 +1,20 @@
+package person
+
+import "fmt"
+
+type Person struct {
+	name string
+	age  int
+}
+
+func (p Person) SetDetails(name string, age int) {
+	p.name = name
+	p.age = age
+	fmt.Println("SetDetails", p)
+}
+
+func (p *Person) SetDetailsPtr(name string, age int) {
+	p.name = name
+	p.age = age
+	fmt.Println("SetDetails", p)
+}
