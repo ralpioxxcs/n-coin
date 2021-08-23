2e6bdf71ed6f8fb09d09b93675156333255fe700
ralpioxxcs <tmfmfkal1123@naver.com>
2021-08-23 23:59:19 +0900

    UPDATE #7 CLI flag

--- main.go
+++ main.go
@@ -1,10 +1,40 @@
 package main
 
 import (
-	"github.com/ralpioxxcs/nocoin/rest"
+	"flag"
+	"fmt"
+	"os"
 )
 
+func usage() {
+	fmt.Printf("Welcome to 노마드 코인\n")
+	fmt.Printf("Plase use the following commands:\n\n")
+	fmt.Printf("explorer:	Start the HTML Explorer\n")
+	fmt.Printf("rest: 		Start the REST API (recommended)\n")
+
+	os.Exit(2)
+}
+
 func main() {
-	//go explorer.Start(3000)
-	rest.Start(4000)
+	if len(os.Args) < 2 {
+		usage()
+	}
+
+	rest := flag.NewFlagSet("rest", flag.ExitOnError)
+	portFlag := rest.Int("port", 4000, "Sets the port of the server")
+
+	switch os.Args[1] {
+	case "explorer":
+		fmt.Println("Start Explorer")
+	case "rest":
+		rest.Parse(os.Args[2:])
+	default:
+		usage()
+	}
+
+	if rest.Parsed() {
+		fmt.Println(*portFlag)
+		fmt.Println("Start server")
+	}
+
 }
