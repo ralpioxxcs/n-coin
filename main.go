5aadee721c1bf3d2e0edd60a2658bbdd9fb4385a
ralpioxxcs <tmfmfkal1123@naver.com>
2021-10-25 23:23:44 +0900

    #12 Recap go builtins

--- main.go
+++ main.go
@@ -1,11 +1,36 @@
 package main
 
 import (
-	"github.com/ralpioxxcs/n-coin/cli"
-	"github.com/ralpioxxcs/n-coin/db"
+	"fmt"
+	"time"
 )
 
+func send(c chan<- int) /** send only **/ {
+	for i := range [10]int{} {
+		// time.Sleep(100 * time.Millisecond)
+		fmt.Printf(">> sending %d <<\n", i)
+		c <- i // will block until receive somewhere
+		fmt.Printf(">> sent%d <<\n", i)
+	}
+	close(c)
+}
+
+func receive(c <-chan int) /** receive only **/ {
+	for {
+		time.Sleep(1 * time.Second)
+		a, ok := <-c
+		if !ok {
+			fmt.Println("we're Done!")
+			break
+		}
+		fmt.Printf("|| received %d ||\n", a)
+	}
+}
+
 func main() {
-	defer db.Close() // execute when main() exited
-	cli.Start()
+	//c := make(chan int)    // unbufferd channel
+	c := make(chan int, 10) // bufferd channel (queue)
+
+	go send(c)
+	receive(c)
 }
