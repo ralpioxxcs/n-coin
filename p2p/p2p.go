f368e46f295480c34d11fe131c7cb8f056c83e0d
ralpioxxcs <tmfmfkal1123@naver.com>
2021-11-03 23:37:06 +0900

    #12 Peers map

--- p2p/p2p.go
+++ p2p/p2p.go
@@ -3,7 +3,7 @@ package p2p
 import (
 	"fmt"
 	"net/http"
-	"time"
+	"strings"
 
 	"github.com/gorilla/websocket"
 	"github.com/ralpioxxcs/n-coin/utils"
@@ -12,20 +12,31 @@ import (
 var upgrader = websocket.Upgrader{}
 
 func Upgrade(rw http.ResponseWriter, r *http.Request) {
+	// port :3000 will upgrade the request from :4000
 	upgrader.CheckOrigin = func(r *http.Request) bool {
 		return true
 	}
 	conn, err := upgrader.Upgrade(rw, r, nil)
 	utils.HandleErr(err)
-	for {
-		_, p, err := conn.ReadMessage() // blocking
-		if err != nil {
-			conn.Close()
-			break
-		}
-		fmt.Printf("Just got:%s\n\n", p)
-		time.Sleep(5 * time.Second)
-		message := fmt.Sprintf("New message : %s", p)
-		utils.HandleErr(conn.WriteMessage(websocket.TextMessage, []byte(message)))
-	}
+
+	// openPort is used by other peers to request each other peers
+	openPort := r.URL.Query().Get("openPort")
+	result := strings.Split(r.RemoteAddr, ":")
+	initPeer(conn, result[0], openPort)
+}
+
+/*
+	+----------+	  +----------+
+	|  :4000   | ---> |	  :3000  |
+	+----------+	  +----------+
+	- add peer
+	- make websocket connection to port 3000
+*/
+func AddPeer(address, port, openPort string) {
+	// Port :4000 is requesting an upgrade from the port :3000
+	conn, _, err := websocket.DefaultDialer.Dial(fmt.Sprintf("ws://%s:%s/ws?openPort=%s", address, port, openPort), nil)
+	utils.HandleErr(err)
+
+	initPeer(conn, address, port)
+	// "127.0.0.1:3000" : p{conn}
 }
--- rest/rest.go
+++ rest/rest.go
@@ -48,6 +48,10 @@ type addTxPayload struct {
 	Amount int
 }
 
+type addPeerPayload struct {
+	Address, Port string
+}
+
 func documentation(rw http.ResponseWriter, r *http.Request) {
 	data := []urlDescription{
 		{
@@ -180,6 +184,18 @@ func myWallet(rw http.ResponseWriter, r *http.Request) {
 	*/
 }
 
+func peers(rw http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "POST":
+		var payload addPeerPayload
+		json.NewDecoder(r.Body).Decode(&payload)
+		p2p.AddPeer(payload.Address, payload.Port, port)
+		rw.WriteHeader(http.StatusOK)
+	case "GET":
+		json.NewEncoder(rw).Encode(p2p.Peers)
+	}
+}
+
 func Start(aPort int) {
 	port = fmt.Sprintf(":%d", aPort)
 
@@ -195,6 +211,7 @@ func Start(aPort int) {
 	router.HandleFunc("/wallet", myWallet).Methods("GET")
 	router.HandleFunc("/transactions", transactions).Methods("POST")
 	router.HandleFunc("/ws", p2p.Upgrade).Methods("GET")
+	router.HandleFunc("/peers", peers).Methods("GET", "POST")
 
 	fmt.Printf("Listening on http://localhost%s\n", port)
 	log.Fatal(http.ListenAndServe(port, router))
