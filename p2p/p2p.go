ea3c1bef72b3f5b3738df3841c7ba543fa5a19c8
ralpioxxcs <tmfmfkal1123@naver.com>
2021-11-18 22:28:02 +0900

    #12 read, write

--- p2p/p2p.go
+++ p2p/p2p.go
@@ -3,7 +3,7 @@ package p2p
 import (
 	"fmt"
 	"net/http"
-	"strings"
+	"time"
 
 	"github.com/gorilla/websocket"
 	"github.com/ralpioxxcs/n-coin/utils"
@@ -13,30 +13,35 @@ var upgrader = websocket.Upgrader{}
 
 func Upgrade(rw http.ResponseWriter, r *http.Request) {
 	// port :3000 will upgrade the request from :4000
+	openPort := r.URL.Query().Get("openPort")
+	ip := utils.Splitter(r.RemoteAddr, ":", 0)
 	upgrader.CheckOrigin = func(r *http.Request) bool {
-		return true
+		// openPort is used by other peers to request each other peers
+		return openPort != "" && ip != ""
 	}
+
 	conn, err := upgrader.Upgrade(rw, r, nil)
 	utils.HandleErr(err)
 
-	// openPort is used by other peers to request each other peers
-	openPort := r.URL.Query().Get("openPort")
-	result := strings.Split(r.RemoteAddr, ":")
-	initPeer(conn, result[0], openPort)
+	peer := initPeer(conn, ip, openPort)
+	time.Sleep(20 * time.Second)
+	peer.inbox <- []byte("Hi from 3000!")
 }
 
 /*
-	+----------+	  +----------+
-	|  :4000   | ---> |	  :3000  |
-	+----------+	  +----------+
+	+----------+	  	+----------+
+	|  :4000   | ---> |	 :3000   |
+	+----------+	  	+----------+
 	- add peer
 	- make websocket connection to port 3000
 */
 func AddPeer(address, port, openPort string) {
 	// Port :4000 is requesting an upgrade from the port :3000
-	conn, _, err := websocket.DefaultDialer.Dial(fmt.Sprintf("ws://%s:%s/ws?openPort=%s", address, port, openPort), nil)
+	conn, _, err := websocket.DefaultDialer.Dial(
+		fmt.Sprintf("ws://%s:%s/ws?openPort=%s", address, port, openPort[1:]), nil)
 	utils.HandleErr(err)
 
-	initPeer(conn, address, port)
-	// "127.0.0.1:3000" : p{conn}
+	peer := initPeer(conn, address, port)
+	time.Sleep(10 * time.Second)
+	peer.inbox <- []byte("Hello from port 4000!")
 }
--- p2p/peer.go
+++ p2p/peer.go
@@ -9,15 +9,38 @@ import (
 var Peers map[string]*peer = make(map[string]*peer)
 
 type peer struct {
-	conn *websocket.Conn
+	conn  *websocket.Conn
+	inbox chan []byte
+}
+
+func (p *peer) read() {
+	// delete peer in case of error
+	for {
+		_, m, err := p.conn.ReadMessage() // blocking operation
+		if err != nil {
+			break
+		}
+		fmt.Printf("%s", m)
+	}
+}
+
+func (p *peer) write() {
+	for {
+		m := <-p.inbox // blocking until get a msg from inbox
+		p.conn.WriteMessage(websocket.TextMessage, m)
+	}
 }
 
 func initPeer(conn *websocket.Conn, address, port string) *peer {
+	key := fmt.Sprintf("%s:%s", address, port)
 	p := &peer{
 		conn,
+		make(chan []byte),
 	}
+	go p.read()
+	go p.write()
 
-	key := fmt.Sprintf("%s:%s", address, port)
 	Peers[key] = p
+
 	return p
 }
